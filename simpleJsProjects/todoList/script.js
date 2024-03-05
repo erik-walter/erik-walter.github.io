b8420ff9ac20e7175b89e05e45dc93c2873c4c32
Crudelita <Katha.Walter97@web.de>
2024-03-05 19:31:46 +0100

    todo list add and delete tasks

--- simpleJsProjects/todoList/script.js
+++ simpleJsProjects/todoList/script.js
@@ -1,7 +1,29 @@
-const inputBox = document.getElementsByClassName("textinput");
-const list = document.getElementsByClassName("list-container");
-const button = document.getElementsByClassName("addbutton");
+const inputBox = document.getElementById("textinput");
+const list = document.getElementById("list-container");
+const button = document.getElementById("addbutton");
 
-button.addEventListener("click", function(){
-  
+button.addEventListener('click', (e)=>{
+  if(inputBox.value === ''){
+    alert("You must write something!");
+  }
+  else{
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    list.appendChild(li);
+    inputBox.value = "";
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7"
+    li.appendChild(span)
+  }
+});
+
+list.addEventListener('click', function(e){ 
+  if(e.target.tagname === 'LI'){
+    alert("li");
+    e.target.classList.toggle("checked");
+  }
+  else if(e.target.tagname === 'SPAN'){
+    alert("span");
+    e.target.parentElement.remove();
+  }
 });
