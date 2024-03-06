1f2a5a8a8296e8700baedfaf93019e7620d1a684
Crudelita <Katha.Walter97@web.de>
2024-03-06 14:11:33 +0100

    local Storage

--- simpleJsProjects/todoList/script.js
+++ simpleJsProjects/todoList/script.js
@@ -15,15 +15,26 @@ button.addEventListener('click', (e)=>{
     span.innerHTML = "\u00d7"
     li.appendChild(span)
   }
+  saveData();
 });
 
 list.addEventListener('click', function(e){ 
-  if(e.target.tagname === 'LI'){
-    alert("li");
+  if(e.target.tagName === 'LI'){
     e.target.classList.toggle("checked");
+    saveData();
   }
-  else if(e.target.tagname === 'SPAN'){
-    alert("span");
+  else if(e.target.tagName === 'SPAN'){
     e.target.parentElement.remove();
+    saveData();
   }
 });
+
+function saveData(){
+  localStorage.setItem("data", list.innerHTML)
+}
+
+function showTask(){
+  list.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
